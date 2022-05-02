ec936496df61d5eefa92728676945f579e868a7a
Frans <97081479+Fransssss@users.noreply.github.com>
2022-05-02 02:00:33 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// Title: Average after drop lowest value
+// Purpose: Eliminate lowest value in array and calculate average
+// Have fun - Enjoy the process - Practices makes Improvement
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    double data[] = {10,20,30};
+    // double data[] = {1,2,3};
+    double sum = 0;
+    double low = 0;
+    double countEl = 0;
+    double average = 0;
+    const double size = sizeof(data)/sizeof(double);
+
+    low = data[0];
+    for(size_t i = 0; i< size; ++i)
+    {
+        if(data[i] < low)                  // find the  lowest value
+        {
+            low = data[i];
+        }
+    }
+
+    for(size_t i = 0; i < size; ++i)
+    {
+        if(data[i] > low)                  // sum without the  lowest value
+        {
+            sum += data[i];
+            countEl++;
+        }
+    }
+
+    average = sum/countEl;
+    cout << "average: " << average << endl;
+
+    return 0;
+}
